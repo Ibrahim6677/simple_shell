ead6eac6d503bdf0045ae9f4f1cd2eb4f5a8f6d3
ibrahim <ibrahimsaad6677@gmail.com>
2023-11-09 00:08:17 +0200

    finshed

--- string_function4.c
+++ string_function4.c
@@ -37,7 +37,7 @@ char **strtow(char *str, char *d)
 			for (k = 0; k < j; m++)
 				free(s[k]);
 			free(s);
-			return(NULL);
+			return (NULL);
 		}
 		for (m = 0; m < k; m++)
 			s[j][m] = str[i++];
@@ -58,19 +58,19 @@ char **strtow2(char *str, char d)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	 if (str == NULL || str[0] == 0)
-		 return (NULL);
-	 for (i = 0; str[i] != '\0'; i++)
-		 if ((str[i] != d && str[i + 1] == d) ||
-				 (str[i] != d && !str[i + 1]) || str[i + 1] == d)
-			 numwords++;
-	 if (numwords == 0)
-		 return (NULL);
-	 s = malloc((1 + numwords) * sizeof(char *));
-	 if (!s)
-		 return (NULL);
-	 for (i = 0, j = 0; j < numwords; j++)
-	 {
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+				(str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
 		while (str[i] == d && str[i] != d)
 			i++:
 		k = 0;
@@ -82,11 +82,11 @@ char **strtow2(char *str, char d)
 			for (k = 0; k < j; k++)
 				free(s[k]);
 			free(s);
-			return(NULL);
+			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j] [m] = str[i++];
-		s[j] [m] = 0;
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	 }
 	 s[j] = NULL;
 	 return (s);
