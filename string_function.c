5a87e78b2615f8cd54c359f3cdc8316c9acf35fd
ibrahim <ibrahimsaad6677@gmail.com>
2023-11-10 10:55:42 +0200

    final

--- string_function.c
+++ string_function.c
@@ -2,7 +2,8 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: thr string whose lebgth to check
+ * @s: the string whose length to check
+ *
  * Return: integer length of string
  */
 int _strlen(char *s)
@@ -21,6 +22,7 @@ int _strlen(char *s)
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
  * @s2: the second strang
+ *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
@@ -39,10 +41,11 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - check if needle start with haystack
+ * starts_with - checks if needle starts with haystack
  * @haystack: string to search
  * @needle: the substring to find
- * Return: addressof next char of haystack or NULL
+ *
+ * Return: address of next char of haystack or NULL
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -53,10 +56,11 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatentes two strings
+ * _strcat - concatenates two strings
  * @dest: the destination buffer
  * @src: the source buffer
- * Return: pointer to destinatoin buffer
+ *
+ * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
 {
--- string_function3.c
+++ string_function3.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * *_strncpy - copies a string
- * @dest: the destination string to be copied to
- * @src: the source string
- * @n: the amount of characters to be copied
- * Return: the concatenated string
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -31,11 +31,11 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- * *_strncat - concatenates two string
- * @dest: the first string
- * @src: the second string
- * @n: the amount of bytes to be maximally used
- * Return: the concatenated string
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -44,9 +44,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0' && j < n)
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
@@ -56,10 +58,10 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- * *_strchr - locates a character in a string
- * @s: the strinfg to be parsed
- * @c: the character to look for
- * Return: (s) a pointer to the momery area s
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
